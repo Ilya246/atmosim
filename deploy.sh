da04f5b389a13069ce6c3fca83eadc228e340133
Ilya246 <ilyukarno@gmail.com>
2024-07-24 19:23:58 +0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,9 @@ x86_64-w64-mingw32-g++ atmosim.cpp -o deploy/atmosim.exe -O3 --static --std=c++2
 echo "built atmosim.exe"
 g++ atmosim.cpp -o deploy/atmosim -O3 --static --std=c++20
 echo "built atmosim"
+cd deploy
+zip atmosim_windows.zip atmosim.exe
+echo "packed atmosim_windows.zip"
+tar -czvf atmosim_linux.tar.gz atmosim
+echo "packed atmosim_linux.tar.gz"
+cd ..
