a110c36310ea8432de984918d09199bc959dafca
Arkanic <arkanic@users.noreply.github.com>
2024-07-27 15:33:56 +1200

    text

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,21 @@
+let outputbox = document.getElementById("outputbox");
 let submit = document.getElementById("submit");
 
 const atmosim = new Worker("./worker_integration.js");
+
+let currentLine = "";
 atmosim.addEventListener("message", (e) => {
     let message = e.data;
     if(message[0] == "output") {
-        console.log(message[1]);
+        let c = message[1];
+        if(c !== "\n") {
+            currentLine = `${currentLine}${c}`;
+        } else {
+            let p = document.createElement("p");
+            p.innerText = currentLine;
+            outputbox.appendChild(p);
+            currentLine = "";
+        }
     }
 });
 
