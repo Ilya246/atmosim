9132a515b805313dd0cd81aea7f29bd0e5f09052
Arkanic <arkanic@users.noreply.github.com>
2024-07-31 21:03:31 +1200

    full output

--- web/src/main.js
+++ web/src/main.js
@@ -20,7 +20,6 @@ atmosim.addEventListener("message", (e) => {
         }
     } else if(message[0] == "finish") {
         let result = allText.map(x => x.split("\t").join(""));
-
         let blocks = {};
         let currentlyAppending = "";
         for(let i in result) {
@@ -33,7 +32,7 @@ atmosim.addEventListener("message", (e) => {
             } else {
                 if(currentlyAppending) {
                     let key = line.split(": ")[0]; // initial state etc
-                    let content = line.split(": ")[1];
+                    let content = line.split(": ").slice(1, line.split(": ").length).join(": ");
                     if(content.startsWith("[")) {
                         // array
                         let array = content.slice(1, content.length - (content.endsWith(";") ? 2 : 1)).split("|").map(x => x.trim());
@@ -44,9 +43,9 @@ atmosim.addEventListener("message", (e) => {
                     }
                 }
             }
-
-            displayOutput(blocks);
         }
+
+        displayOutput(blocks);
     }
 });
 
@@ -59,6 +58,18 @@ let thirt1 = document.getElementById("thirt1");
 let thirt2 = document.getElementById("thirt2");
 let ticks = document.getElementById("ticks");
 let doretest = document.getElementById("doretest");
+
+let resultbox = document.getElementById("resultbox");
+let oCanistermix = document.getElementById("o-canistermix");
+let oLeastmols = document.getElementById("o-leastmols");
+let oCanistertemp = document.getElementById("o-canistertemp");
+let oCanisterpressure = document.getElementById("o-canisterpressure");
+let oTankmix = document.getElementById("o-tankmix");
+let oTankmols = document.getElementById("o-tankmols");
+let oTanktemp = document.getElementById("o-tanktemp");
+let oTankpressure = document.getElementById("o-tankpressure");
+let oTimer = document.getElementById("o-timer");
+let oRadius = document.getElementById("o-radius");
 
 submit.addEventListener("click", e => {
     atmosim.postMessage(["compute", {
@@ -75,5 +86,93 @@ submit.addEventListener("click", e => {
 });
 
 function displayOutput(data) {
-    let requirements = data["REQUIREMENTS"];
+    console.log(data);
+
+    // canister
+    // 60.633331%:39.366669%=1.540220 plasma:tritium
+    {
+        let mixdata = data["REQUIREMENTS"]["mix-canister (fuel)"][0];
+        let [percentdata, typedata] = mixdata.split(" ");
+
+        percentdata = percentdata.split("=")[0];
+        let [firstPercent, secondPercent] = percentdata.split(":").map(x => x.substr(0, x.length - 1)).map(x => parseFloat(x));
+
+        let [firstGas, secondGas] = typedata.split(":");
+
+        oCanistermix.innerText = `Mix: %${firstPercent.toFixed(4)} ${firstGas} : %${secondPercent.toFixed(4)} ${secondGas}`
+    }
+
+    // totally messed up, atmosim output is not very nice to parse
+    {
+        let firstLeastMols = data["REQUIREMENTS"]["mix-canister (fuel)"][3].split(": [")[1];
+        let secondLeastMols = data["REQUIREMENTS"]["mix-canister (fuel)"][4];
+
+        oLeastmols.innerText = `Least mols: ${firstLeastMols}, ${secondLeastMols}`;
+    }
+
+    // temp 398.105682K
+    {
+        let tempStep1 = data["REQUIREMENTS"]["mix-canister (fuel)"][1].split(" ")[1];
+        temp = parseFloat(tempStep1.slice(0, tempStep1.length - 1));
+
+        oCanistertemp.innerText = `Temp: ${temp.toFixed(4)}K`;
+    }
+
+    // release pressure 693.503296kPa
+    {
+        let pressureStep1 = data["REQUIREMENTS"]["mix-canister (fuel)"][2];
+        let pressure = parseFloat(pressureStep1.slice(0, pressureStep1.length - 3));
+
+        oCanisterpressure.innerText = `Canister release pressure set to ${pressure.toFixed(4)}KPa`;
+    }
+
+    // tank
+    // mix
+    {
+        let mix = data["REQUIREMENTS"]["third-canister (primer)"][2].split(" ")[2];
+
+        oTankmix.innerText = `Mix: ${mix}`;
+    }
+
+    // temp 202.005005K
+    {
+        let tempStep1 = data["REQUIREMENTS"]["third-canister (primer)"][0].split(" ")[1];
+        let temp = parseFloat(tempStep1.slice(0, tempStep1.length - 1));
+
+        oTanktemp.innerText = `Temp: ${temp.toFixed(4)}K`;
+    }
+
+    // least-mols: 1115.087769mol oxygen
+    {
+        let leastMols = data["REQUIREMENTS"]["third-canister (primer)"][2].split(": ")[1];
+
+        oTankmols.innerText = `Least mols: ${leastMols}`;
+    }
+
+    // pressure 1337.757357kPa
+    {
+        let pressureStep1 = data["REQUIREMENTS"]["third-canister (primer)"][1].split(" ")[1];
+        let pressure = parseFloat(pressureStep1.slice(0, pressureStep1.length - 3));
+
+        oTankpressure.innerText = `Tank pressure: ${pressure.toFixed(4)}KPa`;
+    }
+
+    // Generic results
+    // ticks 30t
+    {
+        let tickStep1 = data["TANK"]["end state"][0].split(" ")[1];
+        let ticks = parseFloat(tickStep1.slice(0, tickStep1.length - 1));
+
+        oTimer.innerText = `Time before detonation: ${ticks} ticks (${ticks / 2} seconds)`;
+    }
+
+    // radius 11.661351til
+    {
+        let radiusStep1 = data["TANK"]["end state"][3].split(" ")[1];
+        let radius = parseFloat(radiusStep1.slice(0, radiusStep1.length - 3));
+
+        oRadius.innerText = `Explosive radius: ${radius.toFixed(2)} tiles`;
+    }
+
+    resultbox.classList.remove("hidden");
 }
