70633e0c0b59f00b55c1658488a44a769c15f3d8
Ilya246 <57039557+Ilya246@users.noreply.github.com>
2024-12-19 09:57:50 +0400

    -p

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-mkdir deploy
+mkdir -p deploy
 x86_64-w64-mingw32-g++ atmosim.cpp -o deploy/atmosim.exe -O3 --static --std=c++20
 echo "built atmosim.exe"
 g++ atmosim.cpp -o deploy/atmosim -O3 --static --std=c++20
