655d60d3c51d58835b899fc1b97b1b7352a0701a
Ilya246 <ilyukarno@gmail.com>
2025-01-11 12:46:12 +0400

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 mkdir -p deploy
-x86_64-w64-mingw32-g++ atmosim.cpp -o deploy/atmosim.exe -O3 --static --std=c++20
+x86_64-w64-mingw32-g++ *.cpp -o deploy/atmosim.exe -Ofast -flto --static --std=c++20
 echo "built atmosim.exe"
-g++ atmosim.cpp -o deploy/atmosim -O3 --static --std=c++20
+g++ *.cpp -o deploy/atmosim -Ofast -flto --std=c++20
 echo "built atmosim"
 cd deploy
 strip atmosim.exe
